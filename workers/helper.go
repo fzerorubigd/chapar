a92e5a505a5537eff4529057d81364388042da3d
fzerorubigd <fzerorubigd@gmail.com>
2019-02-08 22:32:27 +0100

    remove the example again

--- workers/helper.go
+++ workers/helper.go
@@ -11,3 +11,11 @@ type WorkerFunc func(context.Context, []byte) error
 func (wf WorkerFunc) Process(ctx context.Context, data []byte) error {
 	return wf(ctx, data)
 }
+
+// MiddlewareFunc is the helper for converting a function to middleware
+type MiddlewareFunc func(Worker) Worker
+
+// Wrap is the actual middleware function
+func (mf MiddlewareFunc) Wrap(w Worker) Worker {
+	return mf(w)
+}
